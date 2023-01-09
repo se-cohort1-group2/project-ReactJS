c30c802e8e382052606dab83744f2857cca8241a
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-01-09 20:51:19 +0800

    LTAapi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import "./App.css"; 
 
+import LTAapi from "./api/LTAapi"; 
+
+import { useEffect } from "react"; 
+
 function App() {
+
+    const apiGet = async() => {
+        try {
+            const response = await LTAapi.get("/Taxi-Availability")
+            console.log(response.data.value)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    useEffect(() => {
+        apiGet(); 
+    }, [])
+
     return (
         <>
         <div className="App">
