1dde4a2bbd0ebde82d597c7635a15930a3a5e5f1
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-01-16 18:08:14 +0800

    added TaxiAvailability to LeafletMap

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -3,8 +3,8 @@ import "./LeafletMap.css";
 import { MapContainer, TileLayer } from "react-leaflet"; 
 import ResetViewControl from "@20tab/react-leaflet-resetview"; 
 
-import MapZoom from "./MapZoom"; 
 import TaxiStands from "./TaxiStands"; 
+import TaxiAvailability from "./TaxiAvailability"; 
 
 function LeafletMap() {
     return (
@@ -20,12 +20,13 @@ function LeafletMap() {
                 // copyright attribution, do not remove //
                 attribution='<img src="https://www.onemap.gov.sg/docs/maps/images/oneMap64-01.png" style="height:14px;width:14px;vertical-align:middle;"> <a href="http://onemap.gov.sg" target="_blank" rel="noreferrer">OneMap</a> | © <a href="http://sla.gov.sg" target="_blank" rel="noreferrer">Singapore Land Authority</a>'
                 url="https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png"
+                crossOrigin={true}
             />
             <ResetViewControl
                 icon="url(https://raw.githubusercontent.com/se-cohort1-group2/project-ReactJS/71a66e2ba063dc9ae3a652cac95531e249ea5b71/assets/ResetMapView.svg)"
             />
-            <MapZoom initialZoom={12}/>
-            <TaxiStands initialZoom={12}/>
+                <TaxiStands initialZoom={12}/>
+                <TaxiAvailability initialZoom={12}/>
             </MapContainer>
         </>
     )
