feb898786f664911eb12a6887d71a657d724f27e
OngJinHui <115149035+OngJinHui@users.noreply.github.com>
2023-01-17 22:16:55 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import "./App.css"; 
-
-import LeafletMap from "./components/LeafletMap"; 
+import './App.css';
+// import the API connection
+import ConnAPI from "./api/ConnAPI";
+import CountNearby from './CountNearby';
+import { useState, useEffect } from "react";
 
 function App() {
-    return (
-        <>
-            <LeafletMap/>
-        </>
-    )
+  const [coordPairs, setCoordPairs] = useState([]);
+
+  // function that gets the data from the API
+  const apiGet = async () => {
+    try {
+      const response = await ConnAPI.get("/Taxi-Availability");
+      setCoordPairs(response.data.value);
+      // console.log(response.data.value);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // before the return,
+  // use the useEffect hook to run apiGet when the application starts
+  useEffect(() => {
+    apiGet();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Taxi Availability App</h1>
+      <CountNearby latLon = {coordPairs} />
+    </div>
+  );
 }
 
-export default App; 
+export default App;
