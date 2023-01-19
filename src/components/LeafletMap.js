c883d4c1d1f95e7dc94713698dcf51ac46a36310
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-01-20 07:31:24 +0800

    cleaned up

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -55,8 +55,8 @@ function LeafletMap({
                 <ResetViewControl
                     icon="url(https://raw.githubusercontent.com/se-cohort1-group2/project-ReactJS/71a66e2ba063dc9ae3a652cac95531e249ea5b71/assets/ResetMapView.svg)"
                 />
-                <TaxiStands initialZoom={12} handler={handler}/>
-                <TaxiAvailability initialZoom={12} taxiAvailabilityList={taxiAvailabilityList} handler={handler}/>
+                <TaxiStands initialZoom={12} handler={handler} />
+                <TaxiAvailability initialZoom={12} taxiAvailabilityList={taxiAvailabilityList} handler={handler} />
                 <Polygon pathOptions={{ color: "green" }} positions={polygon} />
                 <SetView center={center} zoom={zoom} />
                 <DetectLocationMarker LocationDetected={LocationDetected} setLocationDetected={setLocationDetected} flyToZoom={flyToZoom} />
