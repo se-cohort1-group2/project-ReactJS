2e5b1d2011e1e6c7ac4e1d42354e4580689b1b54
xong002 <xong002@gmail.com>
2023-01-19 11:12:37 +0800

    removed: Marker from DetectLocationMarker

--- src/components/DetectLocationMarker.js
+++ src/components/DetectLocationMarker.js
@@ -4,32 +4,17 @@ import { Marker, Popup, useMap, Circle } from "react-leaflet";
 function DetectLocationMarker({ LocationDetected, setLocationDetected }) {
 
     const [position, setPosition] = useState(null);
-    const [radius, setRadius] = useState(null);
 
     const map = useMap();
     useEffect(() => {
         if (LocationDetected) {
             map.locate().on("locationfound", function (e) {
                 setPosition(e.latlng);
-                setRadius(e.accuracy);
-                // console.log("Coordinates", e.latlng)
-                // console.log("Accuracy", e.accuracy)
                 map.flyTo(e.latlng, 15);
             })
             setLocationDetected(false);
         }
     }, [LocationDetected, setLocationDetected, map, position])
-
-    return position === null ? null : (
-        <>
-            <Marker position={position} draggable={true} eventHandlers={{
-                click: () => {
-                    map.flyTo(position, map.getZoom());
-                }
-            }}>
-            </Marker>
-        </>
-    )
 }
 
 export default DetectLocationMarker; 
